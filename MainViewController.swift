3a5284467ff7acf3e60287e6aaa43eb5ee615d3c
Gantt Street Baptist Church <ganttstreetbaptistchurch@gsbcs-mac-pro.local>
2015-09-29 22:49:25 -0400

    Init COmmit

---
+++ MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  mainView.swift
+//  Gantt Street Baptist Church
+//
+//  Created by GSBC on 9/29/15.
+//  Copyright © 2015 GSBC. All rights reserved.
+//
+import UIKit
+
+class MainViewController: UIViewController  {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
