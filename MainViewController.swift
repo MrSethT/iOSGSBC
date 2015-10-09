f75c47943d4f30acc4cb75659ac472a40dbc6a05
USC <usc@kryss-mac-mini>
2015-10-08 23:10:48 -0400

    Simplified, almost doing something, added splash text

--- MainViewController.swift
+++ MainViewController.swift
@@ -6,9 +6,16 @@
 //  Copyright © 2015 GSBC. All rights reserved.
 //
 import UIKit
+import AVKit
+import AVFoundation
 
 class MainViewController: UIViewController  {
     
+    @IBAction func btnButton_Click(sender: AnyObject) {
+    }
+    @IBAction func btnVIewLatest_Click(sender: AnyObject) {
+        playVideo()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +25,19 @@ class MainViewController: UIViewController  {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func playVideo(){
+        //let urlVideo :NSURL = NSURL(string: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8")!
+        
+        let urlVideo :NSURL = NSURL(string: "http://stream1.streamatic.com/ganttst.isml/manifest(format=m3u8-aapl).m3u8")!
+        
+        let player  =  AVPlayer(URL: urlVideo)
+        var playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        self.presentViewController(playerViewController, animated:true){
+            playerViewController.player?.play()
+        }
     }
     
     
